a29101fe3051c1f2385f551ef29675effa36bbdf
Nicolas Boisvert <nicklay@me.com>
2018-05-19 00:40:27 -0400

    Publish to hex

--- mix.exs
+++ mix.exs
@@ -4,13 +4,22 @@ defmodule PhpAssocMap.MixProject do
   def project do
     [
       app: :php_assoc_map,
+      version: "0.1.0",
       name: "PhpAssocMap",
       description: "Library that parses PHP's associative array into Elixir's map.",
       source_url: "https://github.com/nicklayb/php_assoc_map",
-      version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Nicolas Boisvert"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nicklayb/php_assoc_map"}
     ]
   end
 
@@ -24,6 +33,7 @@ defmodule PhpAssocMap.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
