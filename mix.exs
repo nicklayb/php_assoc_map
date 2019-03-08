8f267607651d67b30a1473e89e24fffd1bb02aaf
Nicolas Boisvert <nicklay@me.com>
2019-03-08 15:00:05 -0500

    leex - Update versions

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule PhpAssocMap.MixProject do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.8", only: :test},
+      {:excoveralls, "~> 0.10", only: :test},
       {:mix_test_watch, "~> 0.6", only: :dev, runtime: false}
     ]
   end
